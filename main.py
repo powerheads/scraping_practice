e6485e30e258b500db9b6ab59f7c52dd012da354
Oleksander Buryak <niddler86@gmail.com>
2026-02-11 21:43:59 +0200

    Add Kicktraq scraper, handle titles and strip whitespace

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-my_list = [                 1, 2, 3, ]
-print(my_list           )
+import sys
+sys.stdout.reconfigure(encoding="utf-8")
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.kicktraq.com/projects/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+projects = soup.find_all("div", class_="project-pledgilizer-mid green")
+for div in projects:
+    print(div.get_text(strip=True))
+
